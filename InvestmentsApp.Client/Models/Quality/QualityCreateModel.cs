03da0f6ed05526a97d4084be88092bf33abd982d
Дмитрий Богатырев <dbogatirev@joytech.store>
2025-05-11 00:44:27 +0700

    add item type page and quality

---
+++ InvestmentsApp.Client/Models/Quality/QualityCreateModel.cs
@@ -0,0 +1,7 @@
+namespace InvestmentsApp.Client.Models.Quality;
+
+public class QualityCreateModel
+{
+    public string Name { get; set; }
+    public Guid ItemTypeId { get; set; }
+}
---
+++ InvestmentsApp.Client/Models/Quality/QualityViewModel.cs
@@ -0,0 +1,8 @@
+namespace InvestmentsApp.Client.Models.Quality;
+
+public class QualityViewModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public Guid ItemTypeId { get; set; }
+}
